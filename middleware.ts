7a235e30626d1c2facd469d53929068ac0354f5d
Alexander Tarasov <alex.tarasov@sentry.io>
2023-04-05 23:35:19 +0200

    middleware

---
+++ middleware.ts
@@ -0,0 +1,18 @@
+import type { RequestContext } from '@vercel/edge';
+
+// https://vercel.com/docs/concepts/functions/edge-middleware/middleware-api#waituntil
+ 
+const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+ 
+async function getAlbum() {
+  const res = await fetch('https://dev.oioki.me/origin/test.json');
+  await wait(10000);
+  return res.json();
+}
+ 
+export default function middleware(request: Request, context: RequestContext) {
+  context.waitUntil(getAlbum().then((json) => console.log({ json })));
+ 
+  return new Response.json({ hello: 'world' });
+}
+
