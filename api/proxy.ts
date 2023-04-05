e2b467f74cf3b17ade4d34b517018244f98e012f
Alexander Tarasov <alex.tarasov@sentry.io>
2023-04-06 00:19:56 +0200

    try2

--- api/proxy.ts
+++ api/proxy.ts
@@ -3,5 +3,29 @@ export const config = {
 }
 
 export default async (req: Request) => {
-  return new Response("api/proxy response", {status:200});
+  console.log('url', req.url);
+  console.log('query', req.query);
+  console.log('headers', req.headers);
+
+  // Fetch from the backend.
+  const r = await fetch(
+    'https://dev.oioki.me/origin/',
+  )
+
+  const nonce = crypto.randomUUID();
+
+  let csp = r.headers.get('content-security-policy') || '';
+  csp = csp.replace(/MAGIC_NONCE/g, nonce);
+
+  let body = await r.text();
+  body = body.replace(/MAGIC_NONCE/g, nonce);
+
+  return new Response(body, {
+    status: r.status,
+    headers: {
+      // Allow list of backend headers.
+      'content-security-policy': csp,
+      'content-type': r.headers.get('content-type') || '',
+    },
+  })
 }
